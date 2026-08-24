9919c53c0d13cba0ed3c23018896d7f16536cdb9
Manpreet <manpreetjaur11406@gmail.com>
2026-08-24 20:41:47 +0530

    Fix MySQL connection pool

--- config/db.js
+++ config/db.js
@@ -5,11 +5,11 @@ const mysql = require("mysql2");
 // =========================
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST?.trim(),
+  port: Number(process.env.DB_PORT?.trim()),
+  user: process.env.DB_USER?.trim(),
+  password: process.env.DB_PASSWORD?.trim(),
+  database: process.env.DB_NAME?.trim(),
 
   ssl: {
     rejectUnauthorized: true,
